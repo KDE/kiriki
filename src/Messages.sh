e9b3a3c54218a21f50f32e3af9705096439ca08b
Laurent Montel <montel@kde.org>
2008-07-02 06:49:42 +0000

    Remove temporary files
    svn path=/trunk/KDE/kdegames/kiriki/; revision=827038

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,3 +2,4 @@
 $EXTRACTRC *.rc >> rc.cpp || exit 11
 $EXTRACTRC $(ls . | grep -E '\.ui') >> rc.cpp || exit 12
 $XGETTEXT $(ls . | grep -E '\.cpp$') $(ls . | grep -E '\.h$') -o $podir/kiriki.pot
+rm -f rc.cpp
