ae12b53c801535590c420166a909fd70428e046f
Laurent Montel <montel@kde.org>
2009-01-07 20:34:52 +0000

    kdegames compiles with enable final
    svn path=/trunk/KDE/kdegames/kiriki/; revision=907297

--- src/row.h
+++ src/row.h
@@ -6,7 +6,8 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
-
+#ifndef _ROW_H
+#define _ROW_H
 #include <QFlags>
 #include <QString>
 
@@ -48,3 +49,4 @@ class Row
 
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(Row::Flags)
+#endif
